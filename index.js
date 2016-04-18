5d3d0f38af8e6864bc0cd849c9236bbb15bae6cf
Mikel Severson <mkseve@gmail.com>
2016-04-18 15:59:28 -0500

    adds webhook for facebook bot

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.post('/webhook', (req, res) => {
-  res.send('hello world')
+app.get('/webhook', function (req, res) {
+  if (req.query['hub.verify_token'] === 'super-gipfy-secret') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
 })
 
 app.listen(3000, () => {
