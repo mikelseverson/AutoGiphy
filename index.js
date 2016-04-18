5b0da94e205d88ea3c74b938b3328adebe9524e2
Mikel Severson <mkseve@gmail.com>
2016-04-18 15:52:25 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.post('/webhook', (req, res) => {
+  res.send('hello world')
+})
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
