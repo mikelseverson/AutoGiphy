86301760c19d405ddfb6106995d6797830ffe077
Mikel Severson <mikelseverson@users.noreply.github.com>
2016-09-29 23:58:03 -0500

    Update trace.config.js

--- trace.config.js
+++ trace.config.js
@@ -1,3 +1,4 @@
-// trace.config.js
-
-module.exports = { serviceName: 'Frontend API', apiKey: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjU3MTZlYmQxMmFhZDVmMTEwMDhiZmNkNyIsImlhdCI6MTQ2MTExOTk1M30.73FS79KL6FjSEeQguEDOjVaghVxheSwvlq5NBGpcJ08'};
+module.exports = { 
+  serviceName: 'Frontend API', 
+  apiKey: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjU3MTZlYmQxMmFhZDVmMTEwMDhiZmNkNyIsImlhdCI6MTQ2MTExOTk1M30.73FS79KL6FjSEeQguEDOjVaghVxheSwvlq5NBGpcJ08'
+};
